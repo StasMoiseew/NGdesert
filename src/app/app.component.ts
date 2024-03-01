f28250c7aa8bde8c69885c986987341ae78b5e48
smoiseev <smoiseev@aimc.io>
2024-03-01 18:54:16 +0600

    any

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import {HeaderComponent} from "./header/header.component";
 import {SidenavComponent} from "./sidenav/sidenav.component";
 import {HomeComponent} from "./pages/home/home.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import {FaIconComponent, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import {RouterLink} from "@angular/router";
-import { SidenavComponent } from '../sidenav/sidenav.component';
-
+import { HomeComponent } from '../pages/home/home.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    FaIconComponent, FontAwesomeModule, RouterLink, SidenavComponent
+    FaIconComponent, FontAwesomeModule, RouterLink, HomeComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
