e6b2826ed4fbb339ada9ddee9d00bfb2ba48fa61
StasMoiseev <moiseew.kramaritstudio@yandex.ru>
2024-03-03 15:59:41 +0600

    add about c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import {HeaderComponent} from "./header/header.component";
-import {SidenavComponent} from "./sidenav/sidenav.component";
 import {HomeComponent} from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, HeaderComponent, SidenavComponent, HomeComponent,],
+  imports: [RouterOutlet, FontAwesomeModule, HeaderComponent,  HomeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
